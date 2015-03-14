98c6d64ce52b72b31cc7f954ac40a3bd4234567c
rocconnick <dominick.r.rocco@gmail.com>
2015-03-14 17:55:17 -0500

    Merge in NeuralNetwork class with Layer class.

--- NeuralNetwork/__init__.py
+++ NeuralNetwork/__init__.py
@@ -1,3 +1,4 @@
+
 from numpy import matrix
 
 
@@ -21,4 +22,36 @@ class Layer:
     else:
       print "Random initialize"
       #randomInitialize(eps)
+
+
+### Custom exception class for handling problems with architecture 
+class ArchitectureException(Exception):
+  def __init__(self, value):
+    self.value = value
+  def __str__(self):
+    return repr(self.value)
+
+
+class NeuralNetwork:
+  ### Constructor which takes architecture for neural network.  Architecture 
+  ### is specified as a list of integers
+  def __init__(self, arch):
+    self.arch = arch
+    self.layers = []
+    if len(self.arch) < 2:
+      raise ArchitectureException("Must have at least two layers to be a " + 
+                                  "neural network, invalid arcitecture " + 
+                                  "specified: " + str(self.arch))
+                      
+    # Input to second layer will be number of inputs 
+    inputCount = arch[0]
+    for nodeCount in arch[1:]:
+      if not type(nodeCount) == type(3):
+        raise ArchitectureException("Invalid node count:" + str(nodeCount))
+
+      self.layers.append((inputCount, nodeCount))
+      # Input to next layer will be number of nodes in this layer
+      inputCount = nodeCount
+
+
 
--- tests/neuralNetworkConstruction.py
+++ tests/neuralNetworkConstruction.py
@@ -6,9 +6,15 @@ nn = NeuralNetwork.NeuralNetwork([10, 10, 10])
 print "Seems to have worked.\n"
 
 
-print "Trying to make an invalid neural network..."
+print "Trying to make a nonsensical neural network with one layer..."
 try:
   nn = NeuralNetwork.NeuralNetwork([10])
 except NeuralNetwork.ArchitectureException as e:
   print "Good job, we don't allow NNs with one layer, caught exception:\n", e
 
+
+print "Trying to make a decimal neural network..."
+try:
+  nn = NeuralNetwork.NeuralNetwork([10, 3.14])
+except NeuralNetwork.ArchitectureException as e:
+  print "Good job, we don't allow NNs decimal node counts:\n", e
